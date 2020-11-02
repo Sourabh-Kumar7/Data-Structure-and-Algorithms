173bd280db13493235bfe5d3eff93285e26d5f99
Sourabh Kumar <beyourwild7@gmail.com>
2020-11-02 22:25:40 +0530

    Update Pairwise-swap.cpp

--- LinkedList/Pairwise-swap.cpp
+++ LinkedList/Pairwise-swap.cpp
@@ -24,6 +24,34 @@ struct Node {
 }; */
 
 
+node* pairWiseSwap(node* head)
+{
+    if (head == NULL || head->next == NULL)
+        return head;
+  
+    node* prev = head;
+    node* curr = head->next;
+  
+    head = curr; 
+  
+    while (true) {
+        node* next = curr->next;
+        curr->next = prev;
+  
+        if (next == NULL || next->next == NULL) {
+            prev->next = next;
+            break;
+        }
+  
+        prev->next = next->next;
+  
+        prev = next;
+        curr = prev->next;
+    }
+    return head;
+}
+
+
 Node* pairWiseSwap(struct Node* head) {
     Node * temp = head;
     
